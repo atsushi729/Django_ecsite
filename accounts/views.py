a4886aaa2fc617671e74359e90501e30c8add506
atusi <atu729@icloud.com>
2022-05-19 00:32:27 +0900

    add signup form

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from accounts.models import CustomUser
-from accounts.forms import ProfileForm
+from accounts.forms import ProfileForm, SignupUserForm
 from allauth.account import views
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ProfileView(View):
+class ProfileView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         user_data = CustomUser.objects.get(id=request.user.id)
 
@@ -14,7 +15,7 @@ class ProfileView(View):
         })
 
 
-class ProfileEditView(View):
+class ProfileEditView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         user_data = CustomUser.objects.get(id=request.user.id)
         form = ProfileForm(
@@ -56,3 +57,7 @@ class LogoutView(views.LogoutView):
         if self.request.user.is_authenticated:
             self.logout()
         return redirect('/')
+
+class SignupView(views.SignupView):
+    template_name = 'accounts/signup.html'
+    form_class = SignupUserForm
