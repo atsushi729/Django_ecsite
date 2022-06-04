7b137d9f264a5b000f33258fdae4da77c5ce6f19
atusi <atu729@icloud.com>
2022-06-04 23:51:04 +0900

    add substract function

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('product/<slug>', views.ItemDetailView.as_view(), name='product'),
     path('additem/<slug>', views.addItem, name='additem'),
     path('order/', views.OrderView.as_view(), name='order'),
+    path('removeitem/<slug>', views.removeItem, name='removeitem'),
+    path('removesingleitem/<slug>', views.removeSingleItem, name='removesingleitem'),
 ]
 
 
